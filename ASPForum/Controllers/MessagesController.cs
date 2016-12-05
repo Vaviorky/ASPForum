912f9a594d2f9e4a47e49d66c1f7ad0606e78224
LoczekAdam <adamsujeta@gmail.com>
2016-12-05 23:32:28 +0100

    messages and friends

---
+++ ASPForum/Controllers/MessagesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASPForum.Models;
+
+namespace ASPForum.Controllers
+{
+    [Authorize]
+    public class MessagesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Messages
+        public ActionResult Index()
+        {
+            return View(db.Messeges.ToList());
+        }
+
+        // GET: Messages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messeges.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // GET: Messages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Messages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Text,Date,Source")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Messeges.Add(message);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(message);
+        }
+
+        // GET: Messages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messeges.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // POST: Messages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Text,Date,Source")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(message).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(message);
+        }
+
+        // GET: Messages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messeges.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Message message = db.Messeges.Find(id);
+            db.Messeges.Remove(message);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Options()
+        {
+            var id = User.Identity.Name;
+              var qq=  db.Users.Where(u => u.Id == id).Select(u=>u.Id);
+            //var q = db.Friends.Where(f=>f.User.Id==qq).ToList();
+            return PartialView("Options",q);
+        }
+    }
+}
