451f46b3a8524b98e11cd6084057f45ee1ff47e8
Vaviorky <danieloss15@gmail.com>
2016-12-30 17:11:27 +0100

    Selecting options and saving properties into file

--- ASPForum/Controllers/HTMLEditorController.cs
+++ ASPForum/Controllers/HTMLEditorController.cs
@@ -1,66 +1,68 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Web.Mvc;
 using ASPForum.Models;
 
 namespace ASPForum.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class HtmlEditorController : Controller
     {
-        private List<HtmlTagsViewModel> options = new List<HtmlTagsViewModel>();
+        private readonly List<HtmlTagsViewModel> options = new List<HtmlTagsViewModel>();
 
         // GET: HTMLEditor
         public ActionResult Index()
         {
             try
             {
-                var path = AllOptionsInFile();
-                var file = System.IO.File.ReadAllLines(path);
+                var file = System.IO.File.ReadAllLines(AllOptionsInFile());
 
                 foreach (var line in file)
-                {
                     options.Add(new HtmlTagsViewModel
                     {
                         Option = line
                     });
-                }
 
                 using (var sr = new StreamReader(FilePath()))
                 {
                     var contents = sr.ReadToEnd();
+
                     foreach (var option in options)
-                    {
                         if (contents.Contains(option.Option))
-                        {
                             option.IsEnabled = true;
-                        }
-                    }
                 }
             }
-            catch (System.Exception e)
+            catch (Exception e)
             {
                 Debug.WriteLine(e.StackTrace);
             }
             return PartialView(options);
         }
+
         [HttpPost]
         public ActionResult ChangeVaue(IEnumerable<HtmlTagsViewModel> editedoptions)
         {
+            var file = ReadTxtFile(AllOptionsInFile());
 
             foreach (var option in editedoptions)
             {
-                Debug.WriteLine(option.Option + " " + option.IsEnabled);
+                if (option.IsEnabled == false)
+                {
+                    file.Remove(option.Option);
+                }
             }
+            System.IO.File.WriteAllLines(FilePath(), file.Cast<string>());
+
             return RedirectToAction("Index");
         }
 
-        public StringCollection ReadTxtFile()
+        public StringCollection ReadTxtFile(string path)
         {
-            var lines = System.IO.File.ReadAllLines(FilePath());
+            var lines = System.IO.File.ReadAllLines(path);
 
             var wordlist = new StringCollection();
 
@@ -81,12 +83,9 @@ namespace ASPForum.Controllers
             return path;
         }
 
-        private ICollection<HtmlTagsViewModel> GetInfoFromFile()
+        private void ConvertToConfiguration(string path)
         {
-            var collection = new LinkedList<HtmlTagsViewModel>();
-            var file = ReadTxtFile();
-
-            return collection;
+            
         }
     }
 }
